ec88689fd122ac5f3cf9aefc585b110e3ccd9223
S7uy0g <91243828+S7uy0g@users.noreply.github.com>
2022-07-31 19:12:35 +0545

    Update GYM_MANGEMENT_SYSTEM.cpp

--- GYM_MANGEMENT_SYSTEM.cpp
+++ GYM_MANGEMENT_SYSTEM.cpp
@@ -22,44 +22,97 @@ class SanoGraphics
 class Member
 {
 	protected:
-		string name,contact_no;
-		int id,age,height,weight;
+		string Mname,Mcontact_no;
+		int Mid,Mage,Mheight,Mweight;
 	
 	public:
-		void add_members();
-};
-class membership_time: public Member
-{
-	int day,month,year;
-	public:
-		void getMembership();
-};
-void Title_menu()
-{
-	cout<<endl<<endl<<endl<<endl;
-	cout<<"\t\t\t\t\t\t\t\t      =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="<<endl;
-	cout<<"\t\t\t\t\t\t\t\t\t       GYM MANAGEMENT SYSTEM"<<endl;
-	cout<<"\t\t\t\t\t\t\t\t      =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="<<endl;
-}
-int menu(){
-	char ch;
-	cout<<endl<<"\t\t\t\t\t\t\t\t            1) Add Members...............";
-	cout<<endl<<"\t\t\t\t\t\t\t\t            2) List Members..............";
-	cout<<endl<<"\t\t\t\t\t\t\t\t            3) Find Members..............";
-	cout<<endl<<"\t\t\t\t\t\t\t\t            4) Workout Sheet.............";
-	cout<<endl<<"\t\t\t\t\t\t\t\t            5) Trainers Information......";
-	cout<<endl<<"\t\t\t\t\t\t\t\t            6) Equipment Information.....";
-	cout<<endl<<"\t\t\t\t\t\t\t\t            7) Exit......................";
-	cout<<endl<<"\t\t\t\t\t\t\t\t      =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="<<endl;
-	cout<<"\t\t\t\t\t\t\t\t\t      Choice : ";
-	cin>>ch;
-	return ch;
-}
-void choice(char choice){
-	switch(choice){
+		void Title_menu();
+		int menu();
+		void choice(char choice)
+		{
+	    switch(choice)
+		{
 		case '1':
+			int year,month,day;
 			system("cls");
-			cout<<"Hello";
+			cout<<"\n\n\n\n\n\n\n\n"; 
+   	    	cout<<"\n\t\t\t\t\t\t\t\t\t(|--|)Add Member details down bellow(|--|)";
+   	    	cout<<"\n\t\t\t\t\t\t\t\t\t******************************************";
+			cout<<"\n";
+			cout<<"\n\t\t\t\t\t\t\t\t\tName:";
+			getline(cin,Mname);
+			fflush(stdin);
+			cout<<"\t\t\t\t\t\t\t\t\tId:";
+		    cin>>Mid;
+			fflush(stdin); 
+			cout<<"\t\t\t\t\t\t\t\t\tAge:";
+			cin>>Mage;
+			fflush(stdin);
+			cout<<"\t\t\t\t\t\t\t\t\tHeight(in foot):";
+			cin>>Mheight;
+			fflush(stdin);
+			cout<<"\t\t\t\t\t\t\t\t\tWeight(in kg):";
+			cin>>Mweight;
+			fflush(stdin);
+			cout<<"\t\t\t\t\t\t\t\t\tMembership starting date:";
+			cout<<"\n\t\t\t\t\t\t\t\t\tYear:";
+			cin>>year;
+			fflush(stdin);
+			monthwrong:
+			cout<<"\t\t\t\t\t\t\t\t\tMonth:";
+			cin>>month;
+			fflush(stdin);
+			if(month>12||month<1)
+			    {
+			     	cout<<"Wrong Entry!!!!";
+			     	cout<<"\n";
+			     	goto monthwrong;
+				}	 
+			   /*daywrong: 
+			   printf("\t\t\t\t\t\t\t\t\tDay:");
+			   scanf("%d",&dataM.time.day);
+			   fflush(stdin);
+			   if(dataM.time.day>31||dataM.time.day<1)
+			     {
+			     	printf("Wrong Entry!!");
+			     	printf("\n");
+			     	goto daywrong;
+			     }
+			   membershipwrong:	  
+			   printf("\t\t\t\t\t\t\t\t\tChoose a membership plan:");
+			   printf("\n\t\t\t\t\t\t\t\t\t3months");
+			   printf("\n\t\t\t\t\t\t\t\t\t6months");
+			   printf("\n\t\t\t\t\t\t\t\t\t9months");
+			   printf("\n\t\t\t\t\t\t\t\t\t12months");
+			   printf("\n\t\t\t\t\t\t\t\t\tEnter the membership plan(months):");
+			   scanf("%d",&dataM.choicemembership);
+			   if(dataM.choicemembership==3||dataM.choicemembership==6||dataM.choicemembership==9||dataM.choicemembership==12)
+			   { 
+			      int eyear,emonth,tempmonth;
+			      tempmonth=dataM.time.month+dataM.choicemembership;
+	              printf("\n\t\t\t\t\t\t\t\t\tYour package will end after %dmonths prior to this date.",dataM.choicemembership);
+	              if(tempmonth>12)
+	                 {
+	                 	eyear=dataM.time.year+1;
+	                 	emonth=tempmonth-12;
+	                 	printf("\n\t\t\t\t\t\t\t\t\tMembership Ending Date:");
+	                 	printf("\n\t\t\t\t\t\t\t\t\tYear:%d",eyear);
+	                 	printf("\n\t\t\t\t\t\t\t\t\tMonth:%d",emonth);
+	                 	printf("\n\t\t\t\t\t\t\t\t\tDay:%d",dataM.time.day);
+					 }
+			      else
+				     {
+				     	printf("\n\t\t\t\t\t\t\t\t\tMembership Ending Date:");
+	                 	printf("\n\t\t\t\t\t\t\t\t\tYear:%d",dataM.time.year);
+	                 	printf("\n\t\t\t\t\t\t\t\t\tMonth:%d",dataM.time.month+dataM.choicemembership);
+	                 	printf("\n\t\t\t\t\t\t\t\t\tDay:%d",dataM.time.day);
+				     }		 
+			   }
+			   else
+			   {
+			   	 printf("\n\t\t\t\t\t\t\t\t\tThis package doesnot exist!!");
+			   	 goto membershipwrong;
+			   }*/
 		break;
 		case '2':
 			system("cls");
@@ -90,12 +143,42 @@ void choice(char choice){
 			menu();
 	}
 }
+};
+class membership_time: public Member
+{
+	int day,month,year;
+	public:
+		void getMembership();
+};
+void Member::Title_menu()
+{
+	cout<<endl<<endl<<endl<<endl;
+	cout<<"\t\t\t\t\t\t\t\t      =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="<<endl;
+	cout<<"\t\t\t\t\t\t\t\t\t       GYM MANAGEMENT SYSTEM"<<endl;
+	cout<<"\t\t\t\t\t\t\t\t      =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="<<endl;
+}
+int Member::menu(){
+	char ch;
+	cout<<endl<<"\t\t\t\t\t\t\t\t            1) Add Members...............";
+	cout<<endl<<"\t\t\t\t\t\t\t\t            2) List Members..............";
+	cout<<endl<<"\t\t\t\t\t\t\t\t            3) Find Members..............";
+	cout<<endl<<"\t\t\t\t\t\t\t\t            4) Workout Sheet.............";
+	cout<<endl<<"\t\t\t\t\t\t\t\t            5) Trainers Information......";
+	cout<<endl<<"\t\t\t\t\t\t\t\t            6) Equipment Information.....";
+	cout<<endl<<"\t\t\t\t\t\t\t\t            7) Exit......................";
+	cout<<endl<<"\t\t\t\t\t\t\t\t      =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="<<endl;
+	cout<<"\t\t\t\t\t\t\t\t\t      Choice : ";
+	cin>>ch;
+	fflush(stdin);
+	return ch;
+}
 int main(){
 	char c;
 	SanoGraphics dumbbell;
 	dumbbell.graphics();
-	Title_menu();
-	c = menu();
-	choice(c);
+	Member member;
+	member.Title_menu();
+	c = member.menu();
+	member.choice(c);
 	return 0;
 } 
