4b89598a2d76bc52e330cd65b52441ab9b253feb
Rohan Mishra <mishrarohan090@gmail.com>
2022-08-07 20:06:56 +0545

    Obj fixed

--- GYM_MANGEMENT_SYSTEM.cpp
+++ GYM_MANGEMENT_SYSTEM.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stdlib.h>
 #include<iomanip>
 #include<ctime>
 #include<windows.h>
@@ -6,6 +7,7 @@
 using namespace std;
 class SanoGraphics
 {
+	
 	public:
 	void graphics()
 	{
@@ -23,11 +25,15 @@ class SanoGraphics
     	cout<<"\n\t\t\t\t\t\t\t        _________    ___     ___     __     ___   ____   ___     ___";
         cout<<"\n\t\t\t\t\t\t\t       |         |  |   |   |   |   /  \   |   | /    | |   |   /   |";
         cout<<"\n\t\t\t\t\t\t\t       |__     __|  |   |___|   |  /    \  |   |/     | |   |  /   /";
-        cout<<"\n\t\t\t\t\t\t\t          |   |     |    ___    | /  /\  \ |      /|  | |   | /   /"
+        cout<<"\n\t\t\t\t\t\t\t          |   |     |    ___    | /  /\  \ |      /|  | |   | /   /";
+       
 	}*/
 };
 class Member
 {
+	public:
+		string Mname,Mcontact_no;
+		int year,month,day,membership;
 	public:
 		void Title_menu();
 		void menu();
@@ -54,9 +60,9 @@ void Member::menu()
 }
 void Member::Add_Member()
 {
-	string Mname,Mcontact_no;
+	
 	float Mid,Mage,Mheight,Mweight;
-	int year,month,day,membership;
+	
 	int year1, month1, day1;
 	time_t now = time(0);
 	tm *gettime = gmtime(&now);
@@ -122,11 +128,11 @@ void Member::Add_Member()
 	}
 	cout<<"\t\t\t\t\t\t\t\t\t  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="<<endl;
 	membershipwrong:	  
-	cout<<"\t\t\t\t\t\t\t\t\t Choose a membership plan:";
-	cout<<"\n\t\t\t\t\t\t\t\t\t     3months";
-	cout<<"\n\t\t\t\t\t\t\t\t\t     6months";
-	cout<<"\n\t\t\t\t\t\t\t\t\t     9months";
-	cout<<"\n\t\t\t\t\t\t\t\t\t     12months";
+	cout<<"\t\t\t\t\t\t\t\t\t  Choose a membership plan:";
+	cout<<"\n\t\t\t\t\t\t\t\t\t    1) 3months";
+	cout<<"\n\t\t\t\t\t\t\t\t\t    2) 6months";
+	cout<<"\n\t\t\t\t\t\t\t\t\t    3) 9months";
+	cout<<"\n\t\t\t\t\t\t\t\t\t    4) 12months";
 	cout<<"\n\t\t\t\t\t\t\t\t\t     Enter the membership plan(months):";
 	cin>>membership;
 	if(membership==3||membership==6||membership==9||membership==12)
@@ -135,8 +141,8 @@ void Member::Add_Member()
 		tempmonth=month+membership;
 	    cout<<"\n\t\t\t\t\t\t\t\t\tYour package will end after "; 
 		cout<<membership;
-		cout<<"\t\t\t\t\t\t\t\t\tmonths prior to this date.";
-		Sleep(500);
+		cout<<"\n\t\t\t\t\t\t\t\t\tmonths prior to this date.";
+		Sleep(5000);
 	    if(tempmonth>12)
 	    {
 	        eyear=year+1;
@@ -148,7 +154,7 @@ void Member::Add_Member()
 			cout<<emonth;
 	        cout<<"\n\t\t\t\t\t\t\t\t\tDay:";
 			cout<<day;
-			Sleep(2000);
+			Sleep(5000);
 		}
 		else
 		{
@@ -159,23 +165,20 @@ void Member::Add_Member()
 			cout<<month+membership;
 	        cout<<"\n\t\t\t\t\t\t\t\t\tDay:";
 			cout<<day;
-			Sleep(2000);
+			Sleep(5000);
 		}		 
 	}
 	else
 	{
 		cout<<"\n\t\t\t\t\t\t\t\t\tThis package doesnot exist!!";
-		Sleep(2000);
+		Sleep(5000);
 		goto membershipwrong;
 	}
 }
 int main(){
 	int choice;
 	SanoGraphics dumbbell;
-<<<<<<< HEAD
-=======
 	dumbbell.graphics();
->>>>>>> 921f4c447d47038de9c64b035c20099ba64c855b
 	//dumbbell.thankyou();
 	Member member;
 	top:
@@ -186,83 +189,91 @@ int main(){
 	fflush(stdin);
 	switch(choice)
 	{
-	    case 1:
-	    	{
-	    		AddNewMember:
-	    		system("cls");
-	    		int YesNo;	
-	        	fstream file_obj;
-                file_obj.open("E:\\Member.txt",ios::out);
-        		member.Add_Member();
-		        file_obj.write((char*)&member,sizeof(member));
-		        YesNoChoice:
-		        system("cls");
-<<<<<<< HEAD
-		        SanoGraphics TY;
-		        
-=======
->>>>>>> 921f4c447d47038de9c64b035c20099ba64c855b
+	    case 1:	
+	    {
+	    	AddNewMember:
+	    	system("cls");
+	    	int YesNo;	
+	        fstream file_obj;
+            file_obj.open("D:\Member.txt",ios::out);
+            if(file_obj==NULL)
+   	    	{
+   	     		file_obj.open("D:\Member.txt",ios::out);
+   	     		if(file_obj==NULL)
+   	     		{
+   	     	    	printf("File not opened!!!");
+   	     	    	exit(0);
+   	     	   		return 0;
+   	        	}
+	   		}
+        	member.Add_Member();
+		    file_obj.write((char*)&member,sizeof(member));
+		    YesNoChoice:
+		    system("cls");
+	        SanoGraphics TY;
 		       // TY.thankyou();
-		        cout<<"\n\t\t\t\t\t\t\t\t\tDo you want to add another member:";
-		        cout<<"\n\t\t\t\t\t\t\t\t\t1)Yes                        2)No"<<endl;
-		        cout<<"\t\t\t\t\t\t\t\t\tChoice:";
-		        cin>>YesNo;
-		        if(YesNo==1)
-		        {
-		        	system("cls");
-		        	goto AddNewMember;
-				}
-				if(YesNo==2)
-				{
-					system("cls");
-					goto top;
-				}
-				else
-				{
-					cout<<"\n\t\t\t\t\t\t\t\t\tWrong Entry!!";
-					goto YesNoChoice;
-					system("cls");
-				}
-		        break;
+			cout<<"\n\t\t\t\t\t\t\t\t\tDo you want to add another member:";
+		    cout<<"\n\t\t\t\t\t\t\t\t\t1)Yes                        2)No"<<endl;
+		    cout<<"\t\t\t\t\t\t\t\t\tChoice:";
+	        cin>>YesNo;
+		    if(YesNo==1)
+		    {
+		        system("cls");
+		        goto AddNewMember;
 			}
-	    case 2:
-	    	{
-	    		system("cls");
-		        cout<<"Hii";
-		        goto top;
-		        break;	
-			}
-	    case 3:
-	    	{
-	    		system("cls");
-		        cout<<"YoYo";
-		        break;
-			}
-	    case 4:
-	    	{
-	    		system("cls");		
-		        cout<<"Nameste";
-		        break;	
-			}
-    	case 5:
-    		{
-    			system("cls");
-		cout<<"hehe";
-		break;
-			}
-    	case 6:
-    		{
-    				system("cls");
-		cout<<"Hoho";
-    	break;
-			}
-	    default:
-	    	{
-	    		cout<<"\t\t\t\t\t\t Invalid Choice!!!"<<endl;
-				Sleep(1000);
+			if(YesNo==2)
+			{
 				system("cls");
-				
+				goto top;
 			}
+			else
+			{
+				cout<<"\n\t\t\t\t\t\t\t\t\tWrong Entry!!";
+				goto YesNoChoice;
+				system("cls");
+			}
+			break;
+		}
+	    	
+	    case 2:
+			system("cls");
+		    cout<<"Hii";
+			goto top;
+		    break;		
+	    case 3:
+	    {
+	   		system("cls");
+		    cout<<"YoYo";
+			break;
+		}
+	    case 4:
+	   	{
+	    	system("cls");		
+			cout<<"Nameste";
+		    break;	
+		}
+    	case 5:
+    	{
+    		system("cls");
+			cout<<"hehe";
+			break;
+		}
+    	case 6:
+    	{
+    		system("cls");
+			cout<<"Hoho";
+    		break;
+		}
+		case 7:
+		{
+			exit(0);		
+		}
+	    default:
+	    {
+	    	cout<<"\t\t\t\t\t\t Invalid Choice!!!"<<endl;
+			Sleep(1000);
+			system("cls");	
+		}
 	}
 return 0;
 }
